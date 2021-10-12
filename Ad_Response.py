8ce3f25e04e971f6ca6f9f5f9830bc7d08060dd3
The Coding Geek <diabolusregina@gmail.com>
2021-10-12 18:52:06 +0530

    Update Ad_Response.py

--- Ad_Response.py
+++ Ad_Response.py
@@ -1,6 +1,3 @@
-# Logistic Regression
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -9,15 +6,31 @@ from sklearn.linear_model import LogisticRegression
 from matplotlib.colors import ListedColormap
 from matplotlib.colors import ListedColormap
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.cross_validation import train_test_split
+import seaborn as sns
 
 # Importing the dataset
 dataset = pd.read_csv('Dcl_Dgp_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values #independent values
 y = dataset.iloc[:, 4].values #dependent value
 
+
+## Preprocessing: Cleaning the dataset
+
+# Imputer with strategy - mean
+                   
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0) #recognises missing values
+imputer = imputer.fit(X) 
+X= imputer.transform(X)
+
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Understanding the nature of the dataset 
+
+pd.DataFrame(X_test).info()
+pd.DataFrame(X_test).describe()
 
 # Feature Scaling
 
@@ -25,13 +38,20 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
+#Encoding categorical data
+
+le_X = LabelEncoder()
+X_test[:, 0] = le_X.fit_transform(X_test[:, 0]) # encodes vlaues of first column
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X_test = onehotencoder.fit_transform(X_test).toarray()
+le_Y = LabelEncoder()
+
 # Logistic Regression
 
 dcl_clf = LogisticRegression(random_state = 0)
 dcl_clf.fit(X_train, y_train)
-
-# Predicting the Test set results
 pred_dcl = dcl_clf.predict(X_test)
+
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
@@ -72,21 +92,3 @@ plt.xlabel('Age Group')
 plt.ylabel('Salary Range')
 plt.legend()
 plt.show()
-
-## The seaborn plot of the different use-cases
-
-import seaborn as sns
-pd.DataFrame(X_test).info()
-pd.DataFrame(X_test).describe()
-
-print(pred_dcl)
-
-#Encoding categorical data
-
-le_X = LabelEncoder()
-X_test[:, 0] = le_X.fit_transform(X_test[:, 0]) # encodes vlaues of first column
-onehotencoder = OneHotEncoder(categorical_features = [0])
-X_test = onehotencoder.fit_transform(X_test).toarray()
-le_Y = LabelEncoder()
-pred_dcl = le_Y.fit_transform(pred_dcl)
-print(X_test)
